f641175fcf9960889f6d60d0163207a9d08fea76
Luis Bertel <ellipe@gmail.com>
2021-02-16 09:56:56 -0500

    fix ssr rendering with apollo client.

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -1,10 +1,9 @@
 import { ApolloClient, ApolloLink, InMemoryCache } from '@apollo/client';
 import { onError } from '@apollo/link-error';
-import { getDataFromTree } from '@apollo/react-ssr';
+import { getDataFromTree } from '@apollo/client/react/ssr';
 import { createUploadLink } from 'apollo-upload-client';
 import withApollo from 'next-with-apollo';
 import { endpoint, prodEndpoint } from '../config';
-import paginationField from './paginationField';
 
 function createClient({ headers, initialState }) {
   return new ApolloClient({
